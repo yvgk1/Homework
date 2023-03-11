66b3d94cc3fe77b8992e6055342f49cf58e0eea1
yvgk1 <emretopal32@gmail.com>
2023-03-11 13:30:52 +0300

    kodlamaio
    soru1

---
+++ Homework.py
@@ -0,0 +1,24 @@
+print("Metin veri tipleri string olarak tanımlanır.")
+string ="Emre"
+print(string)
+print("Sayısal veri tipleri int,float,complex olarak tanımlanır.")
+int=1 # Tam Sayılar
+float=1,5 # Ondalıklı sayılar
+complex=1j #gerçek ve sanal kısımdan oluşan kompleks sayılar
+print("Boolean değer tipi false yada true değerleri tutmak için kullanılır.")
+boolean=True
+print("Dizi halinde tutulan veriler list,tuple,range olarak ayrılır")
+list=["a","b","c"] 
+tuple=("a","b","c")
+range=range("3")
+
+#Kodlama io sayfasında Kurslarım,Tüm kurslar,Kariyer gibi titlelar string ifadelerdir
+#Kategori ve Eğitmen titleları list ifadelerdir
+#%10 tamamlandı sayaçları '10' integerdir
+
+#Örneğin kursun içeriğine erişmek için üyelik girişi yapılıp yapılmadığını
+#şart blokları ile kontrol edebiliriz
+#if(kulAdi==True && sifre==True)
+#    print("Hoşgeldiniz")
+#else
+#   print(Kullanıcı Girişi Yapınız)
